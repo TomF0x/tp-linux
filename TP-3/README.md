8f8a8a8d2d8451c36510f9ffb44570db60d29a02
TomFox <thomas.rogard@ynov.com>
2021-11-19 12:50:22 +0100

    Push

--- TP-3/README.md
+++ TP-3/README.md
@@ -1,5 +1,30 @@
 # TP 3 : A little script
 
-[![asciicast](https://asciinema.org/a/zAjOwISrOxtWDaCxRFApCgwDf.svg)](https://asciinema.org/a/zAjOwISrOxtWDaCxRFApCgwDf)
+## I. Script carte d'identité
 
-<script id="asciicast-zAjOwISrOxtWDaCxRFApCgwDf" src="https://asciinema.org/a/zAjOwISrOxtWDaCxRFApCgwDf.js" async></script>
+📁 **Fichier** [**`/srv/idcard/idcard.sh`**](https://github.com/TomF0x/tp-linux/blob/main/TP-3/srv/idcard/idcard.sh)
+
+```bash
+tomfox@tomfox:~$ sudo bash idcard.sh 
+Machine name : tomfox
+OS Ubuntu 21.10 and kernel version is Linux 5.13.0-19-generic
+IP: 10.0.2.15
+RAM : 479Mi/1,9Gi
+Disque : 2,8 Go left
+Top 5 processes by RAM usage :
+ - /usr/bin/python3
+ - /usr/lib/xorg/Xorg
+ - xfwm4
+ - /usr/bin/python3
+ - xfdesktop
+Listening ports :
+ - 53 : systemd-r
+ - 631 : cupsd
+ - 22 : sshd
+Here's your random cat : https://cdn2.thecatapi.com/images/a14.jpg
+```
+<img src="https://cdn2.thecatapi.com/images/a14.jpg">
+
+
+## II. Script youtube-dl
+[![asciicast](https://asciinema.org/a/zAjOwISrOxtWDaCxRFApCgwDf.svg)](https://asciinema.org/a/zAjOwISrOxtWDaCxRFApCgwDf)
--- TP-3/srv/idcard/idcard.sh
+++ TP-3/srv/idcard/idcard.sh
@@ -1,12 +1,19 @@
-os=`hostnamectl | grep System`
-kv=`hostnamectl | grep Kernel`
-echo "Machine name : `uname -n`"
+if ! command -v jq &> /dev/null ; then
+        echo "You need to install jq (apt install jq)"
+        exit
+elif ! command -v curl &> /dev/null ; then
+        echo "You need to install curl (apt install curl)"
+        exit
+fi
+os=$(hostnamectl | grep System)  
+kv=$(hostnamectl | grep Kernel)
+echo "Machine name : $(uname -n)"
 echo "OS ${os:18} and kernel version is ${kv:18}"
-echo "IP: `hostname -I | awk '{print $1}'`"
-echo "RAM : `free -h | grep Mem | awk '{print $3}'`/`free -h | grep Mem | awk '{print $2}'`"
-echo "Disque : `df -h / | grep dev | awk '{print $4}' | tr -d 'G'` Go left"
+echo "IP: $(hostname -I | awk '{print $1}')"
+echo "RAM : $(free -h | grep Mem | awk '{print $3}')/$(free -h | grep Mem | awk '{print $2}')"
+echo "Disque : $(df -h / | grep dev | awk '{print $4}' | tr -d 'G') Go left"
 echo "Top 5 processes by RAM usage :"
-echo "`ps -aux --sort -%mem | head -n 6 | tail -n 5 | awk '{print " - " $11}'`"
+ps -aux --sort -%mem | head -n 6 | tail -n 5 | awk '{print " - " $11}'
 echo "Listening ports :"
-echo "`lsof -i -P | grep LISTEN | uniq -w 20 | awk '{print "- " $9 " : " $1}' | tr ":" " " | awk '{print " - " $3 " : " $4}'`"
-echo "Here's your random cat : `curl -s https://api.thecatapi.com/v1/images/search | jq -r ".[].url"`"
+lsof -i -P | grep LISTEN | uniq -w 20 | awk '{print "- " $9 " : " $1}' | tr ":" " " | awk '{print " - " $3 " : " $4}'
+echo "Here's your random cat : $(curl -s https://api.thecatapi.com/v1/images/search | jq -r ".[].url")"
